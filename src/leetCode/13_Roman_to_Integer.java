7315b4a6a8e646d08d67a68e7e5cdcd1a518fd6d
Shamim Ahmed <shamimahmed30082@gmail.com>
2024-05-18 00:00:24 +0600

    day 7 solved problem Roman to Integer

---
+++ src/leetCode/13_Roman_to_Integer.java
@@ -0,0 +1,28 @@
+package leetCode;
+
+class RomanToINteger{
+    public int romanToInt(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int sum = 0;
+        for(int i = 1; i < s.length(); i++){
+
+            if( map.getValue(s.charAt(i)) > map.getValue(s.charAt(i-1)) ){
+                sum = sum + ( map.getValue(s.charAt(i))-map.getValue(s.charAt(i-1)) );
+            }
+            else{
+                sum = sum + ( map.getValue(s.charAt(i)) + map.getValue(s.charAt(i-1)) );
+            }
+            sum = sum + map.getValue(s.charAt(i));
+
+        }
+        return sum;
+}
